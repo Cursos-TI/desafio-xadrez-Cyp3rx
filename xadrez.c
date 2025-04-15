baf8efadcddb7a1c0c72623f062f0c9e48a25ac7
Cyp3rx <138186020+Cyp3rx@users.noreply.github.com>
2025-04-15 22:05:58 +0000

    Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,56 +1,86 @@
 #include <stdio.h>
 
 
+void torres(int torred){
+
+    if (torred <= 5){
+
+        printf("Direita\n");
+
+        torres(torred + 1);
+    }
+}
+
+
+void bispos(int bispod){
+
+    if (bispod <= 5){
+
+        printf("Cima\n");
+
+        for(int bispodir = 1; bispodir <= 1; bispodir++){
+
+            printf("Direita\n");
+        }
+
+        bispos(bispod + 1);
+    }
+}
+
+
+void rainhas(int rainhad){
+
+    if (rainhad <= 8){
+
+        printf("Esquerda\n");
+
+        rainhas(rainhad + 1);
+    }
+}
+
+
 int main(){
 
 
     //declarando as variaveis
     int torre = 1;
     int bispo = 1;
-    int cavalob = 1, cavaloe = 1;
+    int rainha = 1;
 
 
 
     // fazendo os movimentos
     printf("Movimentos da Torre\n");
 
-
-    while(torre <= 5){
-        
-        printf("Direita\n");
-
-        torre++;
-    }
+    torres(torre);
+    
 
     printf("\nMovimentos do Bispo\n");
 
-    do{
+    bispos(bispo);
 
-        printf("Cima,Direita\n");
-
-        bispo++;
-
-    }while(bispo <= 5);
 
     printf("\nMovimentos da Rainha\n");
 
-    for(int rainha = 1; rainha <= 8; rainha++ ){
+    rainhas(rainha);
 
-    printf("Esquerda\n");
-    }
+
 
     printf("\nMovimentos do Cavalo\n");
 
-    for(cavaloe; cavaloe <= 1; cavaloe++){
+    for(int c = 4, d = 0; c > d; c--, d++){
 
-        while(cavalob <= 2){
 
-            printf("baixo\n");
+        if(c >= 0){
 
-        
-            cavalob++;
+            printf("Cima\n");
+
+        } if(d >= 1){
+
+            printf("Direita\n");
         }
+    }
 
         printf("esquerda\n");
-    }
+    
 }  
